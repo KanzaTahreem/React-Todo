d9bb9c9a6ae3ca3edf5b7d27705296fc6313088e
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-10-31 07:39:58 +0500

    Convert class component to function in TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,22 +2,20 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 /* eslint-disable */
-class TodoList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            handleChangeProps={this.props.handleChangeProps}
-            deleteTodoProps={this.props.deleteTodoProps}
-            setUpdate={this.props.setUpdate}
-          />
-        ))}
-      </ul>
-    );
-  }
+const TodoList = (props) => {
+  return (
+    <ul>
+      {props.todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={props.handleChangeProps}
+          deleteTodoProps={props.deleteTodoProps}
+          setUpdate={props.setUpdate}
+        />
+      ))}
+    </ul>
+  );
 }
 
 /* eslint-enable */
